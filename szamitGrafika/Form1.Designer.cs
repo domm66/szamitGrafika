1b8eff0605b59f6dfccada1fcfa791976194b1c7
This dude <This dude@DESKTOP-UCE3M6P>
2021-10-20 12:23:02 +0200

    kb mukodik lol

--- szamitGrafika/Form1.Designer.cs
+++ szamitGrafika/Form1.Designer.cs
@@ -32,6 +32,10 @@ namespace szamitGrafika
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,9 +44,9 @@ namespace szamitGrafika
             this.pictureBox1.Location = new System.Drawing.Point(37, 42);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(562, 362);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // button1
             // 
@@ -64,11 +68,49 @@ namespace szamitGrafika
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(605, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 304);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(605, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(698, 407);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(794, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 304);
+            this.textBox2.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(993, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
@@ -86,6 +128,10 @@ namespace szamitGrafika
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- szamitGrafika/Form1.cs
+++ szamitGrafika/Form1.cs
@@ -22,6 +22,7 @@ namespace szamitGrafika
 
         Image<Bgr, byte> kep;
         Image<Bgr, byte> eredeti;
+        int szamlalo;
         
 
 
@@ -36,8 +37,8 @@ namespace szamitGrafika
             OpenFileDialog ofl = new OpenFileDialog();
             if(ofl.ShowDialog() == DialogResult.OK)
             {
-                eredeti = new Image<Bgr, byte>(ofl.FileName);
-                kep = eredeti.Clone();
+                kep = new Image<Bgr, byte>(ofl.FileName);
+                
                 pictureBox1.Image = kep.ToBitmap();
 
                 //kep = kep.Resize(0.2, Inter.Linear).SmoothMedian(5).SmoothGaussian(1);
@@ -55,8 +56,10 @@ namespace szamitGrafika
         private void button2_Click(object sender, EventArgs e)
         {
             Image<Gray, byte> szurke = kep.Convert<Gray, byte>(); //eredeti kep szurkeve alakitasa
+            
             //CvInvoke.Threshold(szurke, szurke, 115, 255, ThresholdType.Binary);
             CvInvoke.Threshold(szurke, szurke, 0, 255, ThresholdType.Otsu);  //Otsu thresholding, talan kicsit jobban mukodik, de a masikkar is lehet probalkozni, csak ki kell kommentelni
+            Image<Bgr, byte> szurkeSzinben = szurke.Convert<Bgr, byte>();
             Mat hier = new Mat();
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
             VectorOfInt hull = new VectorOfInt();
@@ -65,20 +68,37 @@ namespace szamitGrafika
 
             for (int i = 0; i < contours.Size; i++) 
             {
-                CvInvoke.ConvexHull(contours[i], hull); //convex hull megalkotas
+                CvInvoke.ConvexHull(contours[i], hull,true); //convex hull megalkotas
 
                 for(int j=0;j<hull.Size;j++)
                 {
                     if(j != hull.Size - 1)
                     {
-                        CvInvoke.Circle(eredeti, contours[i][hull[j]], 2, new MCvScalar(200, 0, 50), 2); //pontok kirajzolása
-                        CvInvoke.Line(eredeti, contours[i][hull[j]], contours[i][hull[j + 1]], new MCvScalar(0, 0, 155), 2); //vonalak meghuzasa
+                        int xtav = 15;
+                        int ytav = 10;
+                        int x = contours[i][hull[j]].X;
+                        int y = contours[i][hull[j]].Y;
+                        int xKov = contours[i][hull[j+1]].X;
+                        int yKov = contours[i][hull[j+1]].Y;
+                        if ((x + xtav)<xKov || (y + ytav)<yKov)
+                        {
+                            szamlalo++;
+                            //textBox1.AppendText(j + ". X: " + contours[i][hull[j]].X.ToString() + " Y: " + contours[i][hull[j]].Y.ToString() + "\r\n");
+                            CvInvoke.Circle(szurkeSzinben, contours[i][hull[j]], 5, new MCvScalar(200, 0, 50), 2); //pontok kirajzolása
+                            CvInvoke.Line(szurkeSzinben, contours[i][hull[j]], contours[i][hull[j + 1]], new MCvScalar(0, 0, 155), 2); //vonalak meghuzasa
+
+                        } else {
+                            CvInvoke.Line(szurkeSzinben, contours[i][hull[j]], contours[i][hull[j + 1]], new MCvScalar(0, 0, 155), 2); //vonalak meghuzasa
+                        }
+                        textBox1.AppendText(j + ". X: " + x.ToString() + " Y: " + y.ToString() + "\r\n");
+
+                        
                     }
                     
                 }
             }
-
-            pictureBox1.Image = eredeti.ToBitmap();
+            textBox1.AppendText(szamlalo + "\r\n");
+            pictureBox1.Image = szurkeSzinben.ToBitmap();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -89,6 +109,12 @@ namespace szamitGrafika
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = "X: " + (((this.PointToClient(MousePosition).X - pictureBox1.Location.X))).ToString();
+            label2.Text = "Y: " + (((this.PointToClient(MousePosition).Y - pictureBox1.Location.Y))).ToString();
         }
     }
 }
