10f27497cc9403fc23717bad6fea7ebf6bac5169
This dude <This dude@DESKTOP-UCE3M6P>
2021-11-24 13:36:22 +0100

    a

--- szamitGrafika/Form1.Designer.cs
+++ szamitGrafika/Form1.Designer.cs
@@ -41,6 +41,13 @@ namespace szamitGrafika
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -105,7 +112,7 @@ namespace szamitGrafika
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(113, 23);
             this.button3.TabIndex = 7;
-            this.button3.Text = "Kép töröl";
+            this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
@@ -150,11 +157,89 @@ namespace szamitGrafika
             this.label5.TabIndex = 12;
             this.label5.Text = "Y:";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(709, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Tovább";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(605, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Mappa";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(709, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Auto";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(794, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 46);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "label6";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(794, 164);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(53, 19);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Papír";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(795, 190);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 19);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Olló";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(794, 215);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(40, 19);
+            this.checkBox3.TabIndex = 19;
+            this.checkBox3.Text = "Kő";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(952, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox4);
@@ -190,6 +275,13 @@ namespace szamitGrafika
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
 
--- szamitGrafika/Form1.cs
+++ szamitGrafika/Form1.cs
@@ -14,6 +14,8 @@ using Emgu.Util;
 using Emgu.CV.Structure;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace szamitGrafika
 {
@@ -23,9 +25,16 @@ namespace szamitGrafika
         Image<Bgr, byte> kep;
         Image<Bgr, byte> eredeti;
         int szamlalo;
+        int papir;
+        int ko;
+        int ollo;
         Image<Hsv, byte> imgInput;
+        string path;
+        string[] fileok;
+        int i = 0;
+        int atlag;
 
-
+        //Image<Bgr, byte> kepekFile = new Image<Bgr, byte>(@"C:\Users\This dude\source\repos\szamitGrafika\szamitGrafika\paper");
 
         public Form1()
         {
@@ -34,12 +43,12 @@ namespace szamitGrafika
             textBox4.Text = "13";
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void fileAdd()
         {
             pictureBox1.Image = null;
-            
+
             OpenFileDialog ofl = new OpenFileDialog();
-            if(ofl.ShowDialog() == DialogResult.OK)
+            if (ofl.ShowDialog() == DialogResult.OK)
             {
                 imgInput = new Image<Hsv, byte>(ofl.FileName).Erode(2).Dilate(2);
                 kep = new Image<Bgr, byte>(ofl.FileName);
@@ -52,8 +61,14 @@ namespace szamitGrafika
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            fileAdd();
+        }
+
+        public void convexHull() 
+        {
+            
             pictureBox1.Image = null;
             eredeti = kep.Clone();
             szamlalo = 0;
@@ -65,7 +80,7 @@ namespace szamitGrafika
 
 
             CvInvoke.Threshold(szurke, szurke, 0, 255, ThresholdType.Otsu);  //Otsu thresholding, talan kicsit jobban mukodik, de a masikkar is lehet probalkozni, csak ki kell kommentelni
-            
+
             Image<Bgr, byte> szurkeSzinben = szurke.Convert<Bgr, byte>();
             Mat hier = new Mat();
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
@@ -73,52 +88,62 @@ namespace szamitGrafika
 
             CvInvoke.FindContours(hsv, contours, hier, RetrType.Tree, ChainApproxMethod.ChainApproxNone);
 
-            for (int i = 0; i < contours.Size; i++) 
+            for (int i = 0; i < contours.Size; i++)
             {
-                CvInvoke.ConvexHull(contours[i], hull,true); //convex hull megalkotas
+                CvInvoke.ConvexHull(contours[i], hull, true); //convex hull megalkotas
                 //textBox2.AppendText(hsv.Size.ToString());
 
-                for(int j=0;j<hull.Size;j++)
+                for (int j = 0; j < hull.Size; j++)
                 {
-                    if(j != hull.Size - 1)
+                    if (j != hull.Size - 1)
                     {
                         int xtav = int.Parse(textBox3.Text);
                         int ytav = int.Parse(textBox4.Text);
                         int x = contours[i][hull[j]].X;
                         int y = contours[i][hull[j]].Y;
-                        int xKov = contours[i][hull[j+1]].X;
-                        int yKov = contours[i][hull[j+1]].Y;
-                        if ((x + xtav) < xKov || (y + ytav)<yKov)
+                        int xKov = contours[i][hull[j + 1]].X;
+                        int yKov = contours[i][hull[j + 1]].Y;
+                        if ((x + xtav) < xKov || (y + ytav) < yKov)
                         {
-                            if (x > 10 && x < 180) 
+                            if (x > 10 && x < 180)
                             {
                                 szamlalo++;
                                 CvInvoke.Circle(szurkeSzinben, contours[i][hull[j]], 7, new MCvScalar(200, 0, 50), 2); //pontok kirajzolása
                             }
                             //textBox1.AppendText(j + ". X: " + contours[i][hull[j]].X.ToString() + " Y: " + contours[i][hull[j]].Y.ToString() + "\r\n");
                             CvInvoke.Line(szurkeSzinben, contours[i][hull[j]], contours[i][hull[j + 1]], new MCvScalar(0, 0, 155), 2); //vonalak meghuzasa
-                        } else {
+                        }
+                        else
+                        {
                             //CvInvoke.Circle(eredeti, contours[i][hull[j]], 5, new MCvScalar(200, 0, 50), 2); //pontok kirajzolása
                             CvInvoke.Line(szurkeSzinben, contours[i][hull[j]], contours[i][hull[j + 1]], new MCvScalar(0, 0, 155), 2); //vonalak meghuzasa
                         }
-                        textBox1.AppendText(j + ". X: " + x.ToString() + " Y: " + y.ToString() + "\r\n");
+                        //textBox1.AppendText(j + ". X: " + x.ToString() + " Y: " + y.ToString() + "\r\n");
                     }
                 }
             }
-            if(szamlalo==2)
+            if (szamlalo == 2)
             {
-                label3.Text = "Olló";
+                //textBox1.AppendText("Olló" + "\r\n");
+                ollo++;
             }
-            else if(szamlalo == 5 || szamlalo == 6)
+            else if (szamlalo == 5 || szamlalo == 6)
             {
-                label3.Text = "Papír";
+                //textBox1.AppendText("Papír" + "\r\n");
+                papir++;
             }
             else
             {
-                label3.Text = "Kő";
+                //textBox1.AppendText("Kő" + "\r\n");
+                ko++;
             }
             //textBox1.AppendText(szamlalo + "\r\n");
             pictureBox1.Image = szurkeSzinben.ToBitmap();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            convexHull();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -137,10 +162,89 @@ namespace szamitGrafika
             label2.Text = "Y: " + (((this.PointToClient(MousePosition).Y - pictureBox1.Location.Y))).ToString();
         }
 
-        private void button3_Click_1(object sender, EventArgs e)
+        public void reset()
         {
             szamlalo = 0;
+            papir = 0;
+            ko = 0;
+            ollo = 0;
+            atlag = 0;
+            label6.Text = "";
+            fileok = null;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
             pictureBox1.Image = null;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            imgInput = new Image<Hsv, byte>(fileok[i]).Erode(2).Dilate(2);
+            kep = new Image<Bgr, byte>(fileok[i]);
+            eredeti = kep.Clone();
+            pictureBox1.Image = kep.ToBitmap();
+            i++;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+               
+                //path = dialog.SelectedPath;
+                fileok = Directory.GetFiles(dialog.SelectedPath);
+                //textBox1.Text = dialog.SelectedPath;
+                //textBox1.Text = fileok.Length.ToString();
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //reset();
+            
+            for(int j=0;j<fileok.Length;j++)
+            {
+                pictureBox1.Image = null;
+                imgInput = new Image<Hsv, byte>(fileok[j]).Erode(2).Dilate(2);
+                kep = new Image<Bgr, byte>(fileok[j]);
+                eredeti = kep.Clone();
+                pictureBox1.Image = kep.ToBitmap();
+                convexHull();
+            }
+            
+            //textBox1.Text = papir.ToString();
+            if (checkBox1.Checked)
+            {
+                atlag = papir * 100 / fileok.Length;
+            }
+            else if (checkBox2.Checked)
+            {
+                atlag = ollo * 100 / fileok.Length;
+            }
+            else if (checkBox3.Checked) 
+            {
+                atlag = ko * 100 / fileok.Length;
+            }
+            
+            //textBox1.Text = papir.ToString() + " " + fileok.Length.ToString() + "=" + atlag.ToString();
+            label6.Text = atlag.ToString() + "%";
+           
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
